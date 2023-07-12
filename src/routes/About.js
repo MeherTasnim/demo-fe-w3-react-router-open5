a99e37a124258f521b38249e9f2b683e102431a4
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-07-12 10:54:25 -0500

    Set up basic client-side routing

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return(
+        <div>
+            <h1>I am in the About component!</h1>
+            <p>This is where I talk about what this demo is...</p>
+        </div>
+    )
+}
+
+export { About };
---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return(
+        <div>
+            <h1>I am in the home component!</h1>
+        </div>
+    )
+}
+
+export { Home };
