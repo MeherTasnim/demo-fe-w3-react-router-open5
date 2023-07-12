d1024ddd8f89e1dc0d934bc03a5de6b548a04dd9
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-07-12 11:04:47 -0500

    Add links in components

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function About() {
     return(
         <div>
             <h1>I am in the About component!</h1>
             <p>This is where I talk about what this demo is...</p>
+            <Link to='/'>Click me to go back Home</Link>
         </div>
     )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return(
-        <div>
+        <div className="text-primary">
             <h1>I am in the home component!</h1>
+            {/* <a href="/about">Click me to go to About! (using anchor tag)</a>
+            <br /> */}
+            <Link to="about">Click to go to About (using Link component)</Link>
         </div>
     )
 }
